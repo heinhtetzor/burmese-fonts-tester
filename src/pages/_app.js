4437ae8e2687c2c4cbfe94be20ff25284a8aa68f
heinhtetzor <heinhzor@gmail.com>
2023-11-22 21:13:13 +0800

    Added meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
+import Head from 'next/head' 
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head> 
+                <title>Burmese Font Tester</title>
+                <meta property="description" content="Burmese Font Tester Website" />
+                <meta property="keywords" content="Myanmar Fonts, Burmese Fonts, Myanmar Unicode" />
+
+                <meta property="og:title" content="Burmese Font Tester" />
+                <meta property="og:description" content="Test Unicode and Non-Unicode Fonts" />
+                <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/my/thumb/d/d2/Pamgram.JPG/1280px-Pamgram.JPG" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
